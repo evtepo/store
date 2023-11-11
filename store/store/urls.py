2c968ebe5940b873aee161f5799b3de57133c427
evtepo <andrey-laguta@mail.ru>
2023-11-11 22:31:31 +1000

    Added path for django-debug-toolbar

--- store/store/urls.py
+++ store/store/urls.py
@@ -23,6 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", include("clothes.urls")),
     path("admin/", admin.site.urls),
+
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
