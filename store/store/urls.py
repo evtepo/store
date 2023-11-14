4bb7cb71a80b7252f8dc7c7284925741543d1089
evtepo <andrey_laguta00@mail.ru>
2023-11-14 16:23:32 +1000

    Added Base html file

--- store/store/urls.py
+++ store/store/urls.py
@@ -18,14 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .settings import DEBUG
 
 
 urlpatterns = [
     path("", include("clothes.urls")),
     path("admin/", admin.site.urls),
-
-    path("__debug__/", include("debug_toolbar.urls")),
 ]
+
+if DEBUG:
+    urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
